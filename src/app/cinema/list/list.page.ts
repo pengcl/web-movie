b59c2cfc3845ed4ead54f06b28a0dff7d84ed9a4
pengcl <3559996@qq.com>
2022-09-20 11:58:13 +0800

    finished

--- src/app/cinema/list/list.page.ts
+++ src/app/cinema/list/list.page.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {PopoverController} from '@ionic/angular';
 import {MatDialog} from '@angular/material/dialog';
 import {DataService} from '../../services/data.service';
-import {AppService} from '../../app.service';
 import {CodeComponent} from '../../@theme/entryComponents/code/code';
+import {CinemaService} from '../cinema.service';
 
 @Component({
   selector: 'app-cinema-list',
   templateUrl: './list.page.html',
-  styleUrls: ['./list.page.scss'],
+  styleUrls: ['./list.page.scss']
 })
 export class CinemaListPage implements OnInit {
   movies;
@@ -26,16 +26,20 @@ export class CinemaListPage implements OnInit {
 
   constructor(private popoverController: PopoverController,
               private dialog: MatDialog,
-              private data: DataService,
-              private appSvc: AppService) {
-    this.cinemas = this.getCinemas();
+              private cinemaSvc: CinemaService,
+              private data: DataService) {
+    this.cinemaSvc.find({}).subscribe(res=>{
+      this.cinemas = res;
+    })
     this.data.plans().subscribe(res => {
       this.movies = res.data;
       this.getFilterMovies();
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
   ionScroll(e) {
     this.scrollTop = e.detail.scrollTop;
   }
